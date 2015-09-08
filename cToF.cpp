797122012662d4a83c0d9ac28d265c1d2d5b5209
herlitzj <herlitzj@gmail.com>
2015-09-08 11:41:38 -0400

    c to f conversion

---
+++ cToF.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double temp = 0;
+
+	cout << "What is the temp in Celcius";
+	cin >> temp;
+	cout << "The temp in Farenheit is " << (temp*1.8)+32 << endl;
+
+return 0;
+}

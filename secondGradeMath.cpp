d7f4e1ea9cc1f271fa390fc94ff720f0026cb0ff
herlitzj <herlitzj@gmail.com>
2015-09-08 11:56:41 -0400

    second grade math

---
+++ secondGradeMath.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num1, num2;
+	cout << "What do you want to divide" << endl;
+	cin >> num1;
+	cout << "What do you want to divide it by" << endl;
+	cin >> num2;
+	cout << "That is " << num1 / num2 << " with a remainder of " << num1 % num2 << endl;
+
+return 0;
+
+}

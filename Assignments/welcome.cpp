4dc636a6b31205795dffdfaafe95e1e45f975f9b
herlitzj <herlitzj@gmail.com>
2015-09-10 14:28:14 -0400

    welcome

---
+++ Assignments/welcome.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name;
+
+	cout << "What is your name? ";
+	cin >> name;
+	cout << "******************************" << endl;
+	cout << "Hello " << name << "!" << endl;
+	cout << "Welcome to CSCI-111!!!!!" << endl;
+	cout << "It is great to see you!" << endl;
+	cout << "******************************" << endl;
+
+	return 0;
+
+}
